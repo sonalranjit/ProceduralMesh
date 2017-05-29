3daad7951ba24535c5bb739d445edbd2589e780e
Sonal Ranjit <sonal.ranjit3@gmail.com>
2017-05-29 10:46:06 -0400

    Started Procedural mesh cube

---
+++ Assets/Scripts/ProceduralCube.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(MeshFilter), typeof(MeshRenderer))]
+public class ProceduralCube : MonoBehaviour {
+
+    Mesh mesh;
+    List<Vector3> vertices;
+    List<int> triangles;
+
+    // Use this for initialization
+    private void Awake()
+    {
+        mesh = GetComponent<MeshFilter>().mesh;
+    }
+
+    private void Start()
+    {
+        MakeCube();
+        UpdateMesh();
+    }
+
+    void MakeCube()
+    {
+
+    }
+
+    void MakeFace()
+    {
+        
+    }
+
+    void UpdateMesh()
+    {
+        mesh.Clear();
+        mesh.vertices = vertices;
+        mesh.triangles = triangles;
+        mesh.RecalculateNormals();
+    }
+}
--- Assets/Scripts/ProceduralGrid.cs
+++ Assets/Scripts/ProceduralGrid.cs
@@ -22,7 +22,7 @@ public class ProceduralGrid : MonoBehaviour {
 
     private void Start()
     {
-        MakeDiscreteProceduralGrid();
+        MakeContiguousProceduralGrid();
         UpdateMesh();
     }
 
@@ -62,6 +62,49 @@ public class ProceduralGrid : MonoBehaviour {
         }
 
         
+    }
+
+    void MakeContiguousProceduralGrid()
+    {
+        //setting array sizes
+        vertices = new Vector3[(gridSize + 1) * (gridSize + 1)];
+        triangles = new int[gridSize * gridSize * 6];
+
+        //set tracker integers
+        int v = 0;
+        int t = 0;
+
+        //set vertex offset
+        float vertexOffset = cellSize * 0.5f;
+
+        //create vertex grid
+        for (int x = 0; x <= gridSize; x++)
+        {
+            for (int y = 0; y <= gridSize; y++)
+            {
+                vertices[v] = new Vector3((x * cellSize) - vertexOffset, 0, (y * cellSize) - vertexOffset);
+                v++;
+            }
+
+        }
+
+        // reset vertex tracker
+        v = 0;
+
+        //setting each cell's triangle
+        for (int x = 0; x < gridSize; x++)
+        {
+            for (int y = 0; y < gridSize; y++)
+            {
+                triangles[t] = v;
+                triangles[t + 1] = triangles[t + 4] = v + 1;
+                triangles[t + 2] = triangles[t + 3] = v + (gridSize + 1);
+                triangles[t + 5] = v + (gridSize + 1) + 1;
+                v++;
+                t += 6;
+            }
+            v++;
+        }
     }
 
     void UpdateMesh()
